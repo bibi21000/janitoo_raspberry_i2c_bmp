0cde2327119654623bb4adb35e6dfacd18199e8d
bibi21000 <bibi21000@gmail.com>
2016-04-04 21:44:08 +0200

    Update bmp

--- tests/test_bmp.py
+++ tests/test_bmp.py
@@ -44,6 +44,8 @@ from janitoo.utils import TOPIC_NODES, TOPIC_NODES_REPLY, TOPIC_NODES_REQUEST
 from janitoo.utils import TOPIC_BROADCAST_REPLY, TOPIC_BROADCAST_REQUEST
 from janitoo.utils import TOPIC_VALUES_USER, TOPIC_VALUES_CONFIG, TOPIC_VALUES_SYSTEM, TOPIC_VALUES_BASIC
 
+from janitoo_raspberry_i2c_bmp.bmp import BMPComponent
+
 class TestBMPComponent(JNTTComponent, JNTTComponentCommon):
     """Test the component
     """
